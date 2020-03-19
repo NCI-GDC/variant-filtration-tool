5efbeebac5572565bc21ce3b0527ee25c0a12951
Kyle Hernandez <khernandez@bsd.uchicago.edu>
2020-03-19 18:59:39 +0000

    feat(BINF-346): add somatic score filter

--- gdc_filtration_tools/__main__.py
+++ gdc_filtration_tools/__main__.py
@@ -10,6 +10,7 @@ from gdc_filtration_tools.tools.extract_oxoq import extract_oxoq_from_sqlite
 from gdc_filtration_tools.tools.format_gdc_vcf import format_gdc_vcf
 from gdc_filtration_tools.tools.format_pindel_vcf import format_pindel_vcf
 from gdc_filtration_tools.tools.filter_pos_dkfz import position_filter_dkfz
+from gdc_filtration_tools.tools.filter_somatic_score import filter_somatic_score
 
 
 def main(args: List[str] = None) -> None:
@@ -25,6 +26,7 @@ def main(args: List[str] = None) -> None:
         format_gdc_vcf,
         format_pindel_vcf,
         position_filter_dkfz,
+        filter_somatic_score,
     ]
     defopt.run(funcs, argv=args if args is not None else sys.argv[1:])
     logger.info("Finished!")
