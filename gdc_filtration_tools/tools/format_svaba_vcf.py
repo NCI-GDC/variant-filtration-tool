de5a18d7b89fd6e2992185ba5cf6347e9706da70
linghao <sxwcasd@gmail.com>
2024-09-27 20:46:26 +0000

    reformat

--- gdc_filtration_tools/tools/format_svaba_vcf.py
+++ gdc_filtration_tools/tools/format_svaba_vcf.py
@@ -29,15 +29,17 @@ def format_svaba_vcf(input_vcf: str, origin_vcf: str, output_vcf: str) -> None:
     # setup
     total = 0
     reader = pysam.VariantFile(input_vcf)
-    #origin_vcf_gz = gzip.open(origin_vcf)
-    #header = pysam.VariantFile(origin_vcf_gz)
+    # origin_vcf_gz = gzip.open(origin_vcf)
+    # header = pysam.VariantFile(origin_vcf_gz)
     mode = get_pysam_outmode(output_vcf)
     header = reader.header
     header.formats.remove_header("GQ")
-    header.add_line('##FORMAT=<ID=GQ,Number=1,Type=Float,Description="Genotype quality (SvABA currently not supported. Always 0)">')
+    header.add_line(
+        '##FORMAT=<ID=GQ,Number=1,Type=Float,Description="Genotype quality (SvABA currently not supported. Always 0)">'
+    )
     writer = pysam.VariantFile(output_vcf, mode=mode, header=header)
     print(header)
-    #import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     # Process
     try:
         for record in reader.fetch():
