4104dbd1d93ee519bb6fd7231b82f7cb877e612a
Tzintzuni Garcia <tzintzuni@uchicago.edu>
2025-10-16 22:36:02 +0000

    added a non-pysam vcf parser class in readvcf.py added a tool to format and filter strelka structural variant vcf files in process of adding unit tests

---
+++ gdc_filtration_tools/tools/format_strelka_vcf.py
@@ -0,0 +1,174 @@
+"""
+Format Strelka2 VCF output and perform additional quality filtration.
+
+1. Remove the existing incorrect genotype from the Normal sample
+2. Add corrected germline genotype to Normal sample from NT tag using the conversion table:
+    - ref → 0/0
+    - het → 0/1
+    - hom → 1/1
+    - conflicts → ./.
+3. For INDELs add correct somatic genotype to Tumor sample from SGT tag using the above table
+4. For SNVs set somatic genotype to 0/1
+5. Add a hard line filter on QSI tag values of <= 10 for INDELs
+6. Ensure GT format specification exists in header
+"""
+
+from typing import List, Tuple
+
+from pysam import tabix_compress, tabix_index
+
+from gdc_filtration_tools.logger import Logger
+from gdc_filtration_tools.readvcf import VcfReader
+
+
+def format_strelka_vcf(input_vcf: str, output_vcf: str) -> None:
+    """
+    Processes Strelka2 VCFs to add GT calls in standard format and adds a conservative quality
+    filter to remove obviously incorrect variant calls.
+
+    :param input_vcf: The input VCF file to undo the Picard header fix.
+    :param output_vcf: The output formatted VCF file to create. BGzip and tabix-index created if ends with '.gz'.
+    """
+
+    vcf = VcfReader(input_vcf)
+    vcf.header["FORMAT"] = ensure_gt(vcf.header["FORMAT"])
+    temp_vcf = "temp.vcf"
+
+    with open(temp_vcf, "wt") as outvcf:
+        # write header
+        for line in vcf.iter_header_lines():
+            print(line, file=outvcf)
+        # adjust and write rows
+        for row in vcf.iter_rows():
+            new_row = adjust_record(row)
+            line = "\t".join(new_row)
+            print(line, file=outvcf)
+
+    if output_vcf.enswith(".gz"):
+        tabix_compress(temp_vcf, output_vcf)
+        os.remove(temp_vcf)
+    else:
+        os.rename(temp_vcf, output_vcf)
+    tabix_index(output_vcf, preset="vcf")
+
+
+def ensure_gt(format_section: List[str]) -> List[str]:
+    """
+    Ensure GT format specification exists in header
+    """
+    format_section["GT"] = (
+        '##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">'
+    )
+    return format_section
+
+
+def adjust_record(row: Tuple) -> Tuple:
+    """
+    Orchestrate adjustments to individual record
+    """
+    adjust_fn = get_indel_or_snp_fn(row)
+    return adjust_fn(row)
+
+
+def adjust_SNV(row: Tuple) -> Tuple:
+    """
+    Extract germline GT from NT INFO field
+    Set somatic GT to 0/1
+    """
+    # extract values from strings
+    n_values = row.NORMAL.split(":")[1:]
+    t_values = row.TUMOR.split(":")[:-1]
+    # compute germline and somatic GT
+    info = parse_info(row.INFO)
+    germline_GT = convert_gt_spec(info["NT"])
+    somatic_GT = "0/1"
+    # build replacement strings
+    n_str = ":".join([germline_GT] + n_values)
+    t_str = ":".join([somatic_GT] + t_values)
+    return row._replace(NORMAL=n_str, TUMOR=t_str)
+
+
+def adjust_INDEL(rec: Tuple) -> Tuple:
+    """
+    Extract somatic GT from NT INFO field
+    Extract germline GT from SGT INFO field
+    Filter on QSI
+    """
+    # extract values from strings
+    n_values = row.NORMAL.split(":")[1:]
+    t_values = row.TUMOR.split(":")[:-1]
+    # compute germline and somatic GT
+    info = parse_info(row.INFO)
+    germline_GT = convert_gt_spec(info["NT"])
+    sgt_somatic = info["SGT"].split("->", 1)[1]
+    somatic_GT = convert_gt_spec(sgt_somatic)
+    # build replacement strings
+    n_str = ":".join([germline_GT] + n_values)
+    t_str = ":".join([somatic_GT] + t_values)
+    return row._replace(NORMAL=n_str, TUMOR=t_str)
+
+
+def convert_gt_spec(strelka_gt: str) -> str:
+    """
+    Converts the strelka genotype to conventional format:
+
+    ref -> 0/0
+    het -> 0/1
+    hom -> 1/1
+    conflicts -> ./.
+
+    :param strelka_gt: The strelka genotype as a string
+    """
+    conversion = {"ref": "0/0", "het": "0/1", "hom": "1/1", "conflict": "./."}
+    return conversion[strelka_gt]
+
+
+def get_indel_or_snp_fn(row: Tuple) -> str:
+    indel_info_key_set = {
+        "IC",
+        "IHP",
+        "QSI",
+        "OVERLAP",
+        "QSI_NT",
+        "RC",
+        "RU",
+        "TQSI",
+        "TQSI_NT",
+    }
+    snv_info_key_set = {
+        "ACGTNacgtnMINUS",
+        "ACGTNacgtnPLUS",
+        "DP",
+        "QSS",
+        "QSS_NT",
+        "ReadPosRankSum",
+        "SNVSB",
+        "TQSS",
+        "TQSS_NT",
+    }
+    common_key_set = {
+        "MQ",
+        "MQ0",
+        "NT",
+        "SGT",
+        "SOMATIC",
+        "SomaticEVS",
+    }
+    info = parse_info(row.INFO)
+    if set(info.keys()) - indel_info_key_set == common_key_set:
+        return adjust_INDEL
+    if set(info.keys()) - snv_info_key_set == common_key_set:
+        return adjust_SNV
+    # TODO: raise exception
+
+
+def parse_info(info_string):
+    return dict(map(parse_field, row.INFO.split(";")))
+
+
+def parse_field(fstring):
+    res = fstring.split("=", 1)
+    if len(res) == 2:
+        return tuple(res)
+    else:
+        return (res[0], True)
---
+++ tests/test_readvcf.py
@@ -0,0 +1,257 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+from gdc_filtration_tools.readvcf import VcfReader, VcfSectionTracker
+
+
+class TestVcfSectionTracker(TestCase):
+    def test_expected_sections(self):
+        vst = VcfSectionTracker()
+        sections = {
+            "fileformat",
+            "FILTER",
+            "FORMAT",
+            "INFO",
+            "INDIVIDUAL",
+            "SAMPLE",
+            "contig",
+        }
+        assert sections == vst.expected_header_sections
+
+    def test_id_header_sections(self):
+        vst = VcfSectionTracker()
+        sections = {
+            "FILTER",
+            "FORMAT",
+            "INFO",
+            "INDIVIDUAL",
+            "SAMPLE",
+            "contig",
+        }
+        assert sections == vst.id_header_sections
+
+    def test_init(self):
+        """
+        Test init sets appropriate values and has expected interface
+        """
+        vst = VcfSectionTracker()
+        public_methods = [
+            "get_line_section",
+            "get_line_id",
+            "update_section",
+            "matches_current",
+            "get_current_section",
+        ]
+        for method in public_methods:
+            assert hasattr(vst, method)
+            assert callable(getattr(vst, method))
+        assert vst._current_section is None
+        assert vst._misc_section_counter == 0
+        assert vst._section_id_counter == 0
+
+    @patch.object(VcfSectionTracker, "_not_misc_section")
+    def test__get_line_section_expected_header(self, nmsfn):
+        """
+        Test behavior when line section is in expected set
+        """
+        vst = VcfSectionTracker()
+        line = "##FILTER="
+        expected_result = "FILTER"
+
+        res = vst.get_line_section(line)
+        assert res == expected_result
+        nmsfn.assert_not_called()
+
+    @patch.object(VcfSectionTracker, "_not_misc_section", return_value=True)
+    def test__get_line_section_initial_unexpected_header(self, nmsfn):
+        """
+        Test behavior when line section is unexpected and current
+        section is not set
+        """
+        vst = VcfSectionTracker()
+        vst._current_section = None
+        line = "##UNEXPECTED="
+        expected_result = "misc_0"
+
+        res = vst.get_line_section(line)
+        assert res == expected_result
+        assert getattr(vst, "_misc_section_counter") == 1
+        nmsfn.assert_called_once()
+
+    @patch.object(VcfSectionTracker, "_not_misc_section", return_value=False)
+    def test__get_line_section_subsequent_unexpected_header(self, nmsfn):
+        """
+        Test behavior when line section is unexpected and current
+        section is already a misc_N section
+        """
+        vst = VcfSectionTracker()
+        vst._current_section = "misc_0"
+        vst._misc_section_counter = 1
+        line = "##UNEXPECTED="
+        expected_result = "misc_0"
+
+        res = vst.get_line_section(line)
+        assert res == expected_result
+        assert getattr(vst, "_misc_section_counter") == 1
+        nmsfn.assert_called_once()
+
+    def test__not_misc_section_no_current_section(self):
+        """
+        Test behavior when self._current_section is None
+        """
+        vst = VcfSectionTracker()
+        vst._current_section = None
+
+        res = vst._not_misc_section()
+        assert res is True
+
+    def test__not_misc_section_misc_current_section(self):
+        """
+        Test behavior when self._current_section is a misc. section
+        """
+        vst = VcfSectionTracker()
+        vst._current_section = "misc_0"
+
+        res = vst._not_misc_section()
+        assert res is False
+
+    def test__not_misc_section_other_current_section(self):
+        """
+        Test behavior when self._current_section is a named section
+        """
+        vst = VcfSectionTracker()
+        vst._current_section = "other"
+
+        res = vst._not_misc_section()
+        assert res is True
+
+    def test_get_line_id_known_section(self):
+        """
+        Test behavior when an ID is present in a section where it's expected
+        """
+        vst = VcfSectionTracker()
+        line = "<ID=thing1>"
+        section = "FILTER"
+
+        res = vst.get_line_id(line, section)
+        assert res == "thing1"
+
+    def test_get_line_no_id_known_section(self):
+        """
+        Test behavior when an ID is present in a section where it's expected
+        """
+        vst = VcfSectionTracker()
+        line = "<>"
+        section = "FILTER"
+
+        res = vst.get_line_id(line, section)
+        assert res == 0
+
+    def test_get_line_no_id_unknown_section(self):
+        """
+        Test behavior when an ID is present in a section where it's expected
+        """
+        vst = VcfSectionTracker()
+        line = "<>"
+        section = "unknown"
+
+        res = vst.get_line_id(line, section)
+        assert res == 0
+
+    def test_update_section(self):
+        """
+        Test update section
+        """
+        vst = VcfSectionTracker()
+        vst.update_section("new_section")
+
+        assert vst._current_section == "new_section"
+        assert vst._section_id_counter == 0
+
+    def test_matches_current_no_section_set(self):
+        """
+        Test behavior when no current section is set
+        """
+        vst = VcfSectionTracker()
+        vst._current_section = None
+        section = "new_section"
+
+        res = vst.matches_current(section)
+        assert res is True
+        assert vst._current_section == "new_section"
+
+    def test_matches_current_different_section_set(self):
+        """
+        Test behavior when current section is set and is different
+        """
+        vst = VcfSectionTracker()
+        vst._current_section = "different_section"
+        section = "new_section"
+
+        res = vst.matches_current(section)
+        assert res is False
+        assert vst._current_section == "different_section"
+
+    def test_matches_current_same_section_set(self):
+        """
+        Test behavior when current section is set and is same
+        """
+        vst = VcfSectionTracker()
+        vst._current_section = "same_section"
+        section = "same_section"
+
+        res = vst.matches_current(section)
+        assert res is True
+        assert vst._current_section == "same_section"
+
+    def test_get_current_section(self):
+        vst = VcfSectionTracker()
+        vst._current_section = "section1"
+
+        res = vst.get_current_section()
+        assert res == "section1"
+
+
+class TestVcfReader(TestCase):
+    @patch.object(VcfReader, "_get_header")
+    def test__init(self, ghfn):
+        filename = "test.vcf"
+        vr = VcfReader(filename)
+
+        assert vr.header == {}
+        assert vr.filename is filename
+        assert vr.records_offset is None
+        ghfn.assert_called_once()
+
+    @patch.object(VcfReader, "_construct_header_dict")
+    @patch.object(
+        VcfReader, "_get_header_sections", return_value={"name": "header sections"}
+    )
+    def test__get_header(self, get_header_sections, construct_header_dict):
+        filename = "test.vcf"
+        vr = VcfReader(filename)
+
+        get_header_sections.assert_called_once()
+        construct_header_dict.assert_called_once_with({"name": "header sections"})
+
+    records = [("one", ["line1"]), ("one", ["line2"])]
+
+    @patch.object(VcfReader, "_get_header", return_value={})
+    @patch.object(VcfReader, "_read_header_sections", return_value=iter(records))
+    def test__get_header_sections(self, read_header_sections, get_header):
+        filename = "test.vcf"
+        vr = VcfReader(filename)
+
+        header_sections = vr._get_header_sections()
+        assert header_sections == {"one": ["line1", "line2"]}
+        read_header_sections.assert_called_once()
+
+    @patch.object(VcfReader, "_get_header", return_value={})
+    def test__construct_header_dict(self):
+        filename = "test.vcf"
+        vr = VcfReader(filename)
+        header_sections = {"misc_0": ["line1"]}
+
+        expected_dictionary = {"misc_0": {0: "line1"}}
+
+        vr._construct_header_dict(header_sections)
